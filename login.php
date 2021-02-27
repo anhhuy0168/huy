382629320c1066253d04848353e85b41abb9f4e7
anhhuy0168 <66711631+anhhuy0168@users.noreply.github.com>
2021-02-27 14:57:35 +0700

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ $password = $_POST['password'];
     if ($username == 'staff' && $password == '123') {
         header("location:staff.html");
     } elseif ($username == 'admin' && $password == '123456') {
-        header("location:boss1.php");
+        header("location:admin.php");
     } else
     {
         echo "incorrect username and password";
