d92f87b15a1e068690519357260320fd3a5b2b1e
anhhuy0168 <66711631+anhhuy0168@users.noreply.github.com>
2021-02-27 20:18:07 +0700

    Update productsubmit.php

--- productsubmit.php
+++ productsubmit.php
@@ -1,13 +1,10 @@
 <?php
-  $host = "ec2-52-70-67-123.compute-1.amazonaws.com";
-  $port = "5432";
-  $dbname = "dbgcikc8b0oi2e";
-  $user = "bwcqwugsizdbqdo";
-  $password = "cdf3aa4678d3872427da74f1e740ac79f7473d9e56630c4731dea41bcbd3d04e"; 
-  $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
-  $dbconn = pg_connect($connection_string);
-  $query = "select * from product";
-  $result = pg_query($dbconn,$query);
+$connect = pg_connect("host=ec2-52-70-67-123.compute-1.amazonaws.com
+dbname=dbgcikc8b0oi2e
+port=5432
+user=wcqwugsizdbqdo
+password=cdf3aa4678d3872427da74f1e740ac79f7473d9e56630c4731dea41bcbd3d04e
+sslmode=require");
   if ($connect === false) {
   die("ERROR: Could not connect to the database server!");
 } else {
