21847bc0bac02e9a2a944278f8e6f4b17eb6c076
anhhuy0168 <66711631+anhhuy0168@users.noreply.github.com>
2021-02-27 16:10:42 +0700

    Update login.php

--- login.php
+++ login.php
@@ -4,9 +4,8 @@ $username = $_POST['username'];
 $password = $_POST['password'];
     if ($username == 'staff' && $password == '123') {
         header("location:staff.html");
-    } elseif ($username == 'admin' && $password == '123') {
-        header("location:boss .php");
-    } else
+    } 
+else
     {
         echo "incorrect username and password";
         require "login.html";
