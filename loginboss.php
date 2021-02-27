900f8864a72aac1dc9fc8d6963007f43084885ab
anhhuy0168 <66711631+anhhuy0168@users.noreply.github.com>
2021-02-27 16:16:02 +0700

    Update loginboss.php

--- loginboss.php
+++ loginboss.php
@@ -2,10 +2,10 @@
 session_start();
 $username = $_POST['username'];
 $password = $_POST['password'];
-if ($username == 'admin' && $password == '123') {
-    header("location:boss.php");
-}
-    else
+    if ($username == 'admin' && $password == '123') {
+        header("location:boss.php");
+    } 
+else
     {
         echo "incorrect username and password";
         require "login.html";
