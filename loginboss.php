a55a45e84b3f8ea551ded0fbba538989103ff09c
anhhuy0168 <66711631+anhhuy0168@users.noreply.github.com>
2021-02-27 16:18:36 +0700

    Update loginboss.php

--- loginboss.php
+++ loginboss.php
@@ -2,7 +2,7 @@
 session_start();
 $username = $_POST['username'];
 $password = $_POST['password'];
-    if ($username == 'admin' && $password == '123') {
+    if ($username == 'boss' && $password == '123') {
         header("location:boss.php");
     } 
 else
