9ab82ffd7d79e0c2076fb2c369c42d9123be3bda
anhhuy0168 <66711631+anhhuy0168@users.noreply.github.com>
2021-03-01 08:11:29 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -5,18 +5,18 @@ port=5432
 user=wcqwugsizdbqdo
 password=cdf3aa4678d3872427da74f1e740ac79f7473d9e56630c4731dea41bcbd3d04e
 sslmode=require");
-if ($connect === false) {
-  die("ERROR: Could not connect to the database server!");
-} else {
-  $id = $_GET['id'];
-}
-//echo ("Connect successfully!");
-$del =  "delete from product where id ='$id'";
-$data = pg_query($connect,$del);
-if ($data) {
-  echo "<script>alert('Edited succesfully!, Refresh');</script>";
-  header('refresh: 3; url=boss1.php');
-} else {
-  echo ("ERROR + $query") . pg_errormessage($query);
-}
-pg_close($connect);
+if ($connectd === false) {
+      die("ERROR: Could not connect to the database server!");
+    } else {
+      $id = $_POST['id'];
+    }
+    $query = "DELETE FROM Product WHERE id = '$id';";
+    $result = pg_query($connectd, $query);
+    if ($result) {
+      echo "<script>alert('Record delete succesfully!, Refresh');</script>";
+      header("location:staff.html");
+    } else {
+      echo ("ERROR + $query") . pg_errormessage($query);
+    }
+    pg_close($connect);
+?>
